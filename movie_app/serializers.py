98ab03ca06d867da6ce8361752b6abd4e3a2251e
Nur-BACK-END <nurelarynov46@gmail.com>
2025-03-08 18:49:10 +0600

    HW1:создание проекта и моделей

---
+++ movie_app/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Director, Movie, Review
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = ['id', 'name']
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'description', 'duration', 'director']
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'id text movie'.split()
+
+class ReviewDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
---
+++ movie_app/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Director, Movie, Review
+from .serializers import (DirectorSerializer, MovieSerializer, 
+                          MovieDetailSerializer,ReviewSerializer,
+                          ReviewDetailSerializer
+)
+
+
+@api_view(['GET'])
+def director_list_api_view(request):
+    directors = Director.objects.all()
+    data = DirectorSerializer(directors, many=True).data
+
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def director_detail_api_view(request, id):
+    try:
+        director = Director.objects.get(id=id)
+
+    except Director.DoesNotExist:
+        return Response(data={'error': 'Директор не найден'},
+                        status=status.HTTP_404_NOT_FOUND)
+    
+    data = DirectorSerializer(director, many=False).data
+
+    return Response(data=data)
+
+
+@api_view(['GET'])
+def movie_list_api_view(request):
+    movies = Movie.objects.all()
+    data = MovieSerializer(movies, many=True).data
+    return Response(data=data)
+
+@api_view(['GET'])
+def movie_detail_api_view(request, id):
+    try:
+        movie = Movie.objects.get(id=id)
+    except Movie.DoesNotExist:
+        return Response(data={'error': 'Фильм не найден'},
+                        status=status.HTTP_404_NOT_FOUND)
+    data = MovieDetailSerializer(movie, many=False).data
+    return Response(data=data)
+
+@api_view(['GET'])
+def review_list_api_view(request):
+    reviews = Review.objects.all()
+    data = ReviewSerializer(reviews, many=True).data
+    return Response(data=data)
+
+@api_view(['GET'])
+def review_detail_api_view(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(data={'error': 'Review не найдено'},
+                        status=status.HTTP_404_NOT_FOUND)
+    data = ReviewDetailSerializer(review, many=False).data
+    return Response(data=data)
